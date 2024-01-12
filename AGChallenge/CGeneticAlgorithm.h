c1abc99726aad6d269bbc8d593579903d141f6d9
hardwak <ymhere2@gmail.com>
2024-01-12 15:02:46 +0100

    Добавьте файлы проекта.

---
+++ AGChallenge/CGeneticAlgorithm.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CIndividual.h"
+
+class CGeneticAlgorithm
+{
+	void start(int popSize, int crossProb, int mutProb) {
+		
+	}
+};
+
---
+++ AGChallenge/CIndividual.h
@@ -0,0 +1,12 @@
+#pragma once
+class CIndividual
+{
+	//coder
+
+	//fitness();
+
+	//mutate();
+
+	//cross();
+};
+
